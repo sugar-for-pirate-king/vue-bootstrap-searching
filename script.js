a615d42214ccfeb71a3270fb0ac9d4612d882660
philiplambok <philip.geraldi@harukaedu.com>
2019-02-17 13:54:35 +0700

    replace global function to component

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+Vue.component('search-component', {
+  template: '#search-template',
+  data(){
+    return {
+      query: '',
+      results: [],
+      masterData: [
+        'Rem',
+        'Dakara',
+        'Emilia',
+        'Nanahira'
+      ],
+      selected: null
+    }
+  },
+  methods: {
+    searching(){
+      this.results = []
+      this.masterData.forEach((data) => {
+        if(data.includes(this.query)) {
+          this.results.push(data)
+        }
+      })
+    },
+
+    select(event){
+      this.query = event.target.outerText
+      this.results = []
+    }
+  }
+})
+
+
 app = new Vue({
   el: '#app',
   data: {
